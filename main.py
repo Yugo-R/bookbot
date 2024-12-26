f9d04ec8e864b3bddbed6ced46a80127425d55bc
Hugo <hugo.r.roussel@gmail.com>
2024-12-26 22:52:03 +0900

    First set up to read books

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+
+    books_path = "books/"
+    frankenstein_book = "frankenstein.txt"
+
+    frankenstein_book_path = books_path + frankenstein_book
+
+    with open(frankenstein_book_path) as f:
+        file_contents = f.read()
+
+        
+
+        
+  
+
+main()
